2bf07dd4f5e1a85ef4eb54778fd846d5217969ec
c60167 <roger.janssen@nl.abnamro.com>
2019-10-15 09:30:39 +0200

    initial commit of the springboot application

---
+++ springboot-springcloudcontract/src/main/java/com/example/examples/AvailableProfiles.java
@@ -0,0 +1,9 @@
+package com.example.examples;
+
+// todo
+public class AvailableProfiles {
+    public static final String FEIGN = "feign";
+    public static final String LIVE = "live";
+    public static final String REST_CLIENT_TEST = "RestClientTest";
+    public static final String MOCK_MVC_TEST = "MockMvcTest";
+}
